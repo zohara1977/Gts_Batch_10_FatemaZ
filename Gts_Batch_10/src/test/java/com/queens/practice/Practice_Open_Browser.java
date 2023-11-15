7ad8f111a8948a98724551abb88a8fc363e76091
zohara1977 <zohara1977@yahoo.com>
2023-11-14 20:27:40 -0500

    First commit

---
+++ Gts_Batch_10/src/test/java/com/queens/practice/Practice_Open_Browser.java
@@ -0,0 +1,29 @@
+package com.queens.practice;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+public class Practice_Open_Browser {
+
+	public static void main(String[] args) throws Throwable {
+		
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		//driver.navigate().to("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		//driver.navigate().back();
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		Thread.sleep(5000);
+		//driver.findElement(By.xpath("//input[@name='username']")).sendKeys("Admin");
+		driver.findElement(By.name("username")).sendKeys("Admin");
+		driver.findElement(By.xpath("//input[@type='password']")).sendKeys("admin123");
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		
+		
+	}
+
+}
